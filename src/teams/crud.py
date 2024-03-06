d549bed1a997445878d2ea2d289f6ff6849bfe22
Dorstol <olehsofronov@gmail.com>
2024-03-06 18:52:04 +0200

    fix

--- src/teams/crud.py
+++ src/teams/crud.py
@@ -14,7 +14,7 @@ async def get_teams(
     session: AsyncSession,
     title: str,
     project_name: str,
-    status: StatusChoices = "",
+    status: StatusChoices = None,
 ):
     query = select(Team).options(joinedload(Team.members))
     if title:
